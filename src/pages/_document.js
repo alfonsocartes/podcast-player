54505456b1b9028441980def4aa02e796b0660d5
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2023-04-07 09:39:59 +0300

    style: add first main layout

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="p-12">
+        <div className="min-w-full">
+          <h1 className="text-2xl font-bold text-cyan-600 divide-y-2">
+            Podcaster
+          </h1>
+          <hr className="mt-2 border-1 border-slate-300" />
+        </div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
