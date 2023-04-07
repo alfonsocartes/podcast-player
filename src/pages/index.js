684756065350dcb9e6868a45b7b41569dbb2a1c6
Alfonso Cartes <3504951+alfonsocartes@users.noreply.github.com>
2023-04-07 09:45:05 +0300

    feat: fetch podcasts only if > 24h has passed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+export default function Home({ entries }) {
+  const [podcasts, setPodcasts] = useState(entries);
+  return <main>{podcasts.length}</main>;
+}
 
-export default function Home() {
-  return <main></main>;
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+  );
+  const data = await res.json();
+  return {
+    props: { entries: data.feed.entry },
+    // only requested again if more than one day has passed since the last time it was requested
+    revalidate: 60 * 60 * 24,
+  };
 }
